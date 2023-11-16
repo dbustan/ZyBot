04085908356cd1a33393f059ddc441def54d6768
dbustanUCSC <dbustan@ucsc.edu>
2023-11-16 02:39:48 -0800

    added simple response functionality for topia

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+client.on(`ready`, (c) => {
+  console.log(`${c.user.tag} is online.`);
+});
+
+client.on(`messageCreate`, (message) => {
+  if (message.author.bot) return;
+  const authorName = message.author.username;
+  if (authorName === "topiaa.") {
+    if (message.content === "hello") {
+      message.reply("Hey~!");
+    }
+  }
+});
+client.login(process.env.TOKEN);
